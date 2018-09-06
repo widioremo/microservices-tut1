167022ad750b95caa7d66eff9381d001456a88d1
widioremo <widioremo@gmail.com>
2018-09-06 06:11:58 +0300

    fix validation,server errors

--- api/items.py
+++ api/items.py
@@ -1,7 +1,14 @@
-items = {
-    0: {"name": "First item"},
-}
+# items = {
+#     0: {"name": "First item"},
+# }
 
 
-def search() -> list:
-    return items
+# def search() -> list:
+#     return items
+
+from flask_injector import inject
+from services.provider import ItemsProvider
+
+@inject(data_provider=ItemsProvider)
+def search(data_provider) -> list:
+    return data_provider.get()
--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import connexion
+
+from injector import Binder
+from flask_injector import FlaskInjector
 from connexion.resolver import RestyResolver
+
+from services.provider import ItemsProvider
+
+def configure(binder: Binder) -> Binder:
+    binder.bind(
+        ItemsProvider,
+        ItemsProvider([{"Name": "Test 1"}])
+    )
 
 
 if __name__ == '__main__':
-    app = connexion.App(__name__, 9090, specification_dir='swagger/')
+    app = connexion.FlaskApp(__name__, port=9090, specification_dir='swagger/')
     app.add_api('my_super_app.yaml', resolver=RestyResolver('api'))
     # app.add_api('my_super_app.yaml')
-    # app.run(host='0.0.0.0', debug=True,threaded=True)
-    app.run()
+    app.run(host='0.0.0.0', debug=True,threaded=True)
+    # app.run()
